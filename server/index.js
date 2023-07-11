be579f36e50d673540757a06111cadb6d94ee22b
mthng05 <hngminhthu052003@gmail.com>
2023-07-12 05:13:37 +0700

    Database

--- server/index.js
+++ server/index.js
@@ -1,35 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import posts from "./routers/posts.js";
-// import mongoose from "mongoose";
-
-// const app = express();
-// const PORT = process.env.PORT || 5000; // Specify the port number you want to use
-// const URI =
-//   "mongodb+srv://hnmthu21:mthng05@cluster0.bq9x2ci.mongodb.net/?retryWrites=true&w=majority";
-// // Replace with your MongoDB connection string
-
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true, limit: "30mb"}));
-// app.use(cors());
-
-// app.use('/posts', posts);
-
-// mongoose
-//   .connect(URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to MongoDB");
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on port ${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Error connecting to MongoDB", error);
-//   });
-
 import express from "express"; //pour utiliser la bibliothèque ExpressJS
 // import bodyParser from "body-parser";
 import cors from "cors";
