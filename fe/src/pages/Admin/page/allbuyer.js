e5717aadea0d1cb7e27af44537c9993183694a8e
Truong Hoang Kha <kha@gmail.com>
2023-07-28 20:19:09 +0700

    fix bug

---
+++ fe/src/pages/Admin/page/allbuyer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Allbuyer () {
+  return (
+    <div>allbuyer</div>
+  )
+}
--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -20,6 +20,9 @@ module.exports = {
       flexGrow: {
         2: "2",
       },
+      boxShadow: {
+        "model-9": "box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;",
+      },
     },
   },
   corePlugins: {
