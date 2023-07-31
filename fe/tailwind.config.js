4ef64a9b3fc22439da43c17fd45528b7e0c14b3a
Aplus59 <94693152+Aplus59@users.noreply.github.com>
2023-07-31 15:03:21 +0700

    update tailwind
    update color in admin

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -16,6 +16,8 @@ module.exports = {
         "light-grey": "#D2D2D2",
         cultured: "#F5F5F5",
         "philippine-yellow": "#F7CA00",
+        primary: '#3C50E0',
+        secondary: '#80CAEE',
       },
       flexGrow: {
         2: "2",
