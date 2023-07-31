1c60c3e46f9ed5158dea303b958f8abb0196449e
Jouer Solo <117754722+KyoAni100011@users.noreply.github.com>
2023-07-31 21:24:52 +0700

    Add files via upload

---
+++ be/index.js
@@ -0,0 +1,42 @@
+import express from "express"; //pour utiliser la bibliothèque ExpressJS
+// import bodyParser from "body-parser";
+import cors from "cors";
+import "dotenv/config";
+import userRouter from "./routers/userRoute.js";
+import sellerRouter from "./routers/sellerRoute.js";
+import productRouter from "./routers/producRoute.js";
+import pCategoryRouter from "./routers/pCategoryRoute.js";
+import orderRouter from "./routers/orderRoute.js";
+import mongoose from "mongoose";
+
+const app = express(process.env.DATABASE);
+const PORT = process.env.PORT || 5000;
+const URI = process.env.DATABASE;
+
+app.use(express.json()); //yêu cầu đối tượng đầu vào là object json, xử lý POST PUT
+app.use(express.urlencoded({ extended: true, limit: "30mb" })); //yêu cầu đối tượng đầu vào là chuỗi hoặc mảng, xử lý POST PUT
+app.use(cors()); //tắt Same-Origin Policy và cho phép các máy chủ khác truy cập
+
+app.get("/", (req, res) => {
+  res.send("SUCCESS. Hello world from NODE");
+});
+
+// http://localhost:5000/posts
+app.use("/users", userRouter);
+app.use("/sellers", sellerRouter);
+app.use("/buyers", userRouter);
+app.use("/products", productRouter);
+app.use("/pcategories", pCategoryRouter);
+app.use("/orders", orderRouter);
+
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to DB.");
+    app.listen(PORT, () => {
+      console.log(`Server is connecting to ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ be/models/orderModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  products: [{
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      count: Number,
+      color: String,
+    }
+  }], 
+  totalPrice: {type: Number},
+  quantity: {type: Number},
+  orderStatus: {
+    type: String, 
+    default: "Not Processed",
+    enum: [
+      "Not Processed",
+      "Cash on Delivery",
+      "Processing",
+      "Dispatched",
+      "Cancelled",
+      "Delivered",
+    ]
+  },
+  orderDate: {
+    type: Date,
+    default: Date.now,
+  },
+  orderby: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  paymentMethod: {},
+  shippingMethod: {},
+}, {
+  timestamp: true,
+});
+
+export const Order = mongoose.model("Order", orderSchema);
+
