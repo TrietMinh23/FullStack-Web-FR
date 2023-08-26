30851c9c43d376dd8ca99016ff02bf2ccb54ef7d
TrietMinh23 <npmtriet2k3@gmail.com>
2023-08-26 16:09:26 +0700

    fix_report_admin

--- fe/src/pages/Admin/components/Table/TableReport.js
+++ fe/src/pages/Admin/components/Table/TableReport.js
@@ -4,8 +4,9 @@ import {PiMagnifyingGlass} from "react-icons/pi";
 import {AiOutlineCheckCircle} from "react-icons/ai";
 import PopupProcess from "../PopUp/PopupProcess";
 import {updateReport} from "../../../../api/Report/updateReport";
+import PaginationComponent from "../../../Home/components/Pagination";
 
-const Table = ({rows}) => {
+const Table = ({rows, onPageChange, page, totalPages}) => {
   const [perPage, setPerPage] = useState(5); // Số hàng trên mỗi trang
   const [currentPage] = useState(1); // Trang hiện tại
   const [sortColumn, setSortColumn] = useState(""); // Cột hiện tại được sắp xếp
@@ -281,7 +282,7 @@ const Table = ({rows}) => {
           </label>
           <select
             id="rowsPerPage"
-            className="border border-gray-300 rounded-md p-1"
+            className="border border-gray-300 rounded-md p-1 w-12"
             value={perPage}
             onChange={(e) => setPerPage(Number(e.target.value))}
           >
@@ -290,79 +291,8 @@ const Table = ({rows}) => {
             <option value={15}>15</option>
           </select>
         </div>
-        <div className="flex">
-          <a
-            href="/#"
-            className="px-4 py-2 mx-1 text-gray-500 capitalize bg-white rounded-md cursor-not-allowed dark:bg-gray-800 dark:text-gray-600"
-          >
-            <div className="flex items-center -mx-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 mx-1 rtl:-scale-x-100"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M7 16l-4-4m0 0l4-4m-4 4h18"
-                />
-              </svg>
-            </div>
-          </a>
-          <a
-            href="/#"
-            className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200"
-          >
-            1
-          </a>
-          <a
-            href="/#"
-            className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200"
-          >
-            2
-          </a>
-          <a
-            href="/#"
-            className="hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200"
-          >
-            3
-          </a>
-          <a
-            href="/#"
-            className="hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200"
-          >
-            4
-          </a>
-          <a
-            href="/#"
-            className="hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md sm:inline dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200"
-          >
-            5
-          </a>
-          <a
-            href="/#"
-            className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md dark:bg-gray-800 dark:text-gray-200 hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200"
-          >
-            <div className="flex items-center -mx-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 mx-1 rtl:-scale-x-100"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M17 8l4 4m0 0l-4 4m4-4H3"
-                />
-              </svg>
-            </div>
-          </a>
+        <div className="flex w-full justify-end">
+          <PaginationComponent setPage={onPageChange} page={page} totalPage={totalPages}/>
         </div>
       </div>
       {isProcess && (
